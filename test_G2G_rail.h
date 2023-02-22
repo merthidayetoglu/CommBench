94abca2082f3ce312f3af56a8a79fe5bbac4cc25
Mert Hidayetoglu <merthidayetoglu@gmail.com>
2023-02-22 02:15:55 -0500

    scale crusher

--- test_G2G_rail.h
+++ test_G2G_rail.h
@@ -1,35 +1,35 @@
 {
-
-  MPI_Comm comm_rail;
-  MPI_Comm_split(MPI_COMM_WORLD, myid % groupsize, myid / groupsize, &comm_rail);
-
-  int numgroup;
-  int mygroup;
-  MPI_Comm_size(comm_rail, &numgroup);
-  MPI_Comm_rank(comm_rail, &mygroup);
+  int numgroup = numproc / groupsize;
+  int mygroup = myid / groupsize;
+  int mylocalid = myid % groupsize;
 
   printf("myid %d numgroup %d mygroup %d\n", myid, numgroup, mygroup);
-
 
   Type *sendbuf_d;
   Type *recvbuf_d;
 #ifdef PORT_CUDA
   cudaMalloc(&sendbuf_d, count * sizeof(Type));
-  cudaMalloc(&recvbuf_d, count * numgroup * sizeof(Type));
+  cudaMalloc(&recvbuf_d, count * (numgroup - 1) * sizeof(Type));
 #elif defined PORT_HIP
   hipMalloc(&sendbuf_d, count * sizeof(Type));
-  hipMalloc(&recvbuf_d, count * numgroup * sizeof(Type));
+  hipMalloc(&recvbuf_d, count * (numgroup - 1) * sizeof(Type));
 #else
   sendbuf_d = new Type[count];
   recvbuf_d = new Type[count * numgroup];
 #endif
 
   {
-    CommBench::Comm<Type> bench(comm_rail, CommBench::TEST_CAPABILITY);
-    for(int sender = 0; sender < numgroup; sender++)
-      for(int recver = 0; recver < numgroup; recver++)
-        if(sender != recver)
-          bench.add(sendbuf_d, 0, recvbuf_d, sender * count, count, sender, recver);
+    CommBench::Comm<Type> bench(MPI_COMM_WORLD, CommBench::TEST_CAPABILITY);
+    for(int recvgroup = 0; recvgroup < numgroup; recvgroup++)
+      for(int recv = 0; recv < groupsize; recv++) {
+        int numrecv = 0;
+        for(int sendgroup = 0; sendgroup < numgroup; sendgroup++) {
+          int recver = recvgroup * groupsize + recv;
+          int sender = sendgroup * groupsize + recv;
+          if(sendgroup != recvgroup)
+            bench.add(sendbuf_d, 0, recvbuf_d, numrecv * count, count, sender, recver);
+        }
+      }
     bench.report();
     double data = 2 * count * sizeof(Type) / 1.e9 * groupsize * (numgroup - 1);
     double minTime, medTime, maxTime, avgTime;

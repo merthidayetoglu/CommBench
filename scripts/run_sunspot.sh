475121135bab81cfe35b0d494cab0db2519f9ab0
Merth Hidayetoglu <merth@stanford.edu>
2023-03-19 01:18:09 +0000

    benchmark sunspot

--- scripts/run_sunspot.sh
+++ scripts/run_sunspot.sh
@@ -29,21 +29,16 @@ NTOTRANKS=$(( NNODES * NRANKS ))
 echo "NUM_NODES=${NNODES}  TOTAL_RANKS=${NTOTRANKS}  RANKS_PER_NODE=${NRANKS}  THREADS_PER_RANK=${OMP_NUM_THREADS}"
 echo "OMP_PROC_BIND=$OMP_PROC_BIND OMP_PLACES=$OMP_PLACES"
 
-for g in 1
+library=1
+
+for g in 12
 do
 for k in 1
 do
 #for count in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216 33554432 67108864 134217728 268435456
-for count in 268435456
+for count in 1
 do
-  mpiexec -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth ~/CommBench/gpu_tile_compact.sh ./Alltoall 1 $count 10 20 $g $k
-  #mpiexec --pmi pmix -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth -envall ~/CommBench/wrapper_spread.sh ./Alltoall 1 $count 10 20 $g $k
-  #mpiexec --pmi pmix -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth -envall ~/CommBench/gpu_tile_compact.sh ./Alltoall 1 $count 10 20 $g $k
-  #mpiexec --pmi pmix -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth ./Alltoall 1 $count 10 20 $g $k
-  #mpiexec --pmi pmix -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth ./Alltoall 1 $count 10 20 $g $k
-  #mpiexec --pmi pmix -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth ~/CommBench/wrapper_spread.sh ./Alltoall 1 $count 10 20 $g $k
-  #mpiexec -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth -envall gpu_tile_compact.sh ./Alltoall 1 $count 10 20 $g $k
-  #mpiexec -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth ./Alltoall 1 $count 10 20 $g $k
+  mpiexec -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth ~/CommBench/gpu_tile_compact.sh ./Alltoall $library $count 10 20 $g $k
 done
 done
 done

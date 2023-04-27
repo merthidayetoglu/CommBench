15daea2ab3b70d99053ed393877a81d0c8ae2c80
Mert Hidayetoglu <merthidayetoglu@gmail.com>
2023-04-27 14:33:24 -0400

    rm spock and crusher files

--- scripts/submit_crusher.sh
+++
@@ -1,9 +0,0 @@
-
-module reset
-module load craype-accel-amd-gfx90a
-module load PrgEnv-cray
-module load rocm
-
-export MPICH_GPU_SUPPORT_ENABLED=1
-
-salloc -A CHM137_crusher -t 00:30:00 -N 2 --ntasks-per-node=8 --gpus-per-node=8 --gpu-bind=closest

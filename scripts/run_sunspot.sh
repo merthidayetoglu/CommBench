463da44f97a207a3816821fb5242497e1d44c549
Merth Hidayetoglu <merth@stanford.edu>
2023-05-02 07:26:54 +0000

    update Sunspot scripts

--- scripts/run_sunspot.sh
+++ scripts/run_sunspot.sh
@@ -48,7 +48,7 @@ do
 #for count in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216 33554432 67108864 134217728 268435456
 for count in 10000000
 do
-  mpiexec -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth ~/CommBench/gpu_tile_compact.sh ./CommBench $library $pattern $direction $count $warmup $numiter $p $g $k
+  mpiexec -np ${NTOTRANKS} -ppn ${NRANKS} -d ${NDEPTH} --cpu-bind depth gpu_tile_compact.sh ./CommBench $library $pattern $direction $count $warmup $numiter $p $g $k
 done
 done
 done

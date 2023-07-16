c06ae4c7e48e0bf56489fabd5b258b6bb583f03e
Mert Hidayetoglu <merthidayetoglu@gmail.com>
2023-07-16 04:15:50 -0400

    unload darshan module

--- scripts/submit_frontier.sh
+++ scripts/submit_frontier.sh
@@ -3,9 +3,9 @@ module reset
 module load craype-accel-amd-gfx90a
 module load PrgEnv-cray
 module load amd-mixed
-module unload darshan
+module unload darshan-runtime
 
 export MPICH_GPU_SUPPORT_ENABLED=1
 
-salloc -A CHM137 -t 01:00:00 -N 2 --ntasks-per-node=8 --gpus-per-node=8 --gpu-bind=closest --switch=1
+salloc -A CHM137 -t 01:00:00 -N 4 --ntasks-per-node=8 --gpus-per-node=8 --gpu-bind=closest --switch=1
 
--- scripts/submit_perlmutter.sh
+++ scripts/submit_perlmutter.sh
@@ -2,6 +2,5 @@
 module reset
 module load nvhpc
 module load nccl
-module unload xpmem
 
 salloc --nodes 2 --qos interactive --time 01:00:00 --constraint gpu --gpus 4 --account=m4301

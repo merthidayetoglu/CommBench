63b58877ec8eb81e0bb8b90e7cb527a52a667d6a
yuli821 <yuli9@illinois.edu>
2023-12-27 20:55:22 -0600

    testing

--- pyComm/pyComm.cpp
+++ pyComm/pyComm.cpp
@@ -1,8 +1,38 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
-#include "../comm.h"
+//  #include "../comm.h"
 
 namespace py = pybind11;
+
+#include <mpi.h>
+#include <stdio.h> // for printf
+#include <string.h> // for memcpy
+#include <algorithm> // for std::sort
+#include <vector> // for std::vector
+
+namespace CommBench {
+    static int printid = 0;
+    enum library {null, MPI, NCCL, IPC, STAGE, numlib};
+
+    template <typename T>
+    class Comm {
+        public:
+            const library lib;
+            Comm(library lib);
+    }
+}
+
+template <typename T>
+Comm<T>::Comm(library lib) : lib(lib) {
+    int myid;
+    int numproc;
+    MPI_Comm_rank(comm_mpi, &myid);
+    MPI_Comm_size(comm_mpi, &numproc);
+    if(myid == printid) {
+        printf("success.\n");
+    }
+}
+
 
 PYBIND11_MODULE(pyComm, m) {
     py::enum_<CommBench::library>(m, "library")
@@ -14,7 +44,7 @@ PYBIND11_MODULE(pyComm, m) {
         .value("numlib", CommBench::library::numlib);
     py::class_<CommBench::Comm<int>>(m, "Comm")
         .def(py::init<CommBench::library>())
-        .def("add", &CommBench::Comm<int>::add)
-        .def("start", &CommBench::Comm<int>::start)
-        .def("wait", &CommBench::Comm<int>::wait);
+        // .def("add", &CommBench::Comm<int>::add)
+        // .def("start", &CommBench::Comm<int>::start)
+        // .def("wait", &CommBench::Comm<int>::wait);
 }

1779290227ff4a5b1cb136288251a4b54cfaaf25
Mert Hidayetoglu <merth@stanford.edu>
2024-02-16 20:41:44 -0600

    load working MPI version

--- scripts/submit_delta.sh
+++ scripts/submit_delta.sh
@@ -1,7 +1,8 @@
 
 module reset
 module load nccl
+openmpi/4.1.5+cuda
 
 salloc --account=bbkf-delta-gpu --partition=gpuA100x4-interactive \
-  --nodes=2 --gpus-per-node=4 --ntasks-per-node=4 --cpus-per-task=16 \
+  --nodes=4 --gpus-per-node=4 --ntasks-per-node=4 --cpus-per-task=16 \
   --exclusive --mem=0 --time=01:00:00

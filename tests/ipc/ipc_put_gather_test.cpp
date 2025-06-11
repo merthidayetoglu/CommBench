3a8d8bcf0cfd7c96fd4e45fa267f6b3913bead68
agatram <agatram@blake.sandia.gov>
2025-06-11 11:59:19 -0600

    fixed ipc and xccl p2p tests and cmake build files

---
+++ tests/ipc/ipc_put_gather_test.cpp
@@ -0,0 +1,36 @@
+#include "commbench.h"
+
+#define ROOT 0
+#include "validate.h"
+
+using namespace CommBench;
+
+int main() {
+
+  int *sendbuf;
+  int *recvbuf;
+  size_t numbytes = 1e8;
+
+  init();
+  int numproc = CommBench::numproc;
+  int myid = CommBench::myid;
+
+  allocate(sendbuf, numbytes * numproc);
+  allocate(recvbuf, numbytes * numproc);
+
+  Comm<int> test1(IPC);
+  for(int p = 0; p < numproc; p++){
+  	test1.add(sendbuf, 0, recvbuf, p * numbytes, numbytes, p, ROOT);
+  }
+
+  //test1.measure(5, 10, numbytes * numproc);
+
+  validate(sendbuf, recvbuf, numbytes, 1 /*GATHER*/, test1);
+
+  free(sendbuf);
+  free(recvbuf);
+
+  MPI_Finalize();
+
+  return 0;
+}
---
+++ tests/xccl/xccl_test.cpp
@@ -0,0 +1,26 @@
+#include "commbench.h"
+
+using namespace CommBench;
+
+int main() {
+
+  char *sendbuf;
+  char *recvbuf;
+  size_t numbytes = 1e9;
+
+  init();
+  allocate(sendbuf, numbytes);
+  allocate(recvbuf, numbytes);
+
+  Comm<char> test1(NCCL);
+  test1.add(sendbuf, recvbuf, numbytes, 0, 1);
+
+  test1.measure(5, 10);
+
+  free(sendbuf);
+  free(recvbuf);
+
+  MPI_Finalize();
+
+  return 0;
+}

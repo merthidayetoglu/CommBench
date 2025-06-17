d8dd26c3ea5a1ba586dfa26746558367dfd34215
aadhavs99 <aadhavs99@gmail.com>
2025-06-17 14:33:17 -0600

    changed to template for running CommBench with json file configuration, mean to be combined with main.cpp code from cli branch to give users options between cli and json file configuration

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "commbench.h"
+#define ROOT 0
 #include <string>
 #include <vector>
 #include <json/json.h>
 #include <fstream>
 #include <iostream>
+#include <cstdio>
+#include <unordered_map>
+#include <algorithm>
+#include "validate.h"
 
 using namespace CommBench;
 
+enum pattern { p2p, gather, scatter, broadcast, reduce, alltoall, allgather };
 // Function to check if a given flag is present in the command-line arguments
 bool flagPresent(int argc, char* argv[], const std::string& flag) {
     for (int i = 1; i < argc; ++i) {
@@ -20,7 +26,7 @@ bool flagPresent(int argc, char* argv[], const std::string& flag) {
 
 
 int main(int argc, char* argv[]) {
-
+    init();
     std::ifstream file("config_doc.json", std::ifstream::binary);
     if(!file.is_open()) {
 	    std::cerr << "Could not open file!" << std::endl;
@@ -28,55 +34,57 @@ int main(int argc, char* argv[]) {
 
     Json::Value root;
     Json::CharReaderBuilder builder;
-    std::string errs;
-    std::string library = "";
-    std::vector<std::string> patterns;
+    std::string errsg
     int step = 1;
 
     if(!Json::parseFromStream(builder, file, &root, &errs)) {
 	    std::cerr << "Failed to parse Json" << std::endl;
     }
-    if (root.isMember("tests") && root["tests"].isArray()) {
-            Json::Value& testsArray = root["tests"];
-            for (const auto& test : testsArray) {
+
+    library platform = library::MPI;
+    std::vector<pattern> patterns;
+
+    if (root.isMember("steps") && root["steps"].isArray()) {
+            Json::Value& stepsArray = root["steps"];
+            for (const auto& stepNumber : stepsArray) {
 		std::cout << "STEP " << step << std::endl;
 	        step = step + 1;	
-	    	std::string library = test["library"].asString();
-		Json::Value patternsArray = test["patterns"];
-               	if (library == "mpi" || library == "xccl" || library == "ipc_put" || library == "ipc_get") {
-                   std::cout << "entered mpi for loop" << std::endl;
-                   for (const auto& pattern : patternsArray) {
-                       if (pattern.asString() == "gather") {
-                          std::cout << "running gather_test" << std::endl;
-                          std::string command = "mpirun -n 2 unit_tests/" + library + "_gather_test";
-                          int result = system(command.c_str());
-                       } else if (pattern.asString() == "p2p") {
-                                 std::cout << "running p2p_test" << std::endl;
-                                 std::string command = "mpirun -n 2 unit_tests/" + library + "_p2p_test";
-                                 int result = system(command.c_str());
-                       } else if (pattern.asString() == "scatter") {
-                                 std::string command = "mpirun -n 2 unit_tests/" + library + "_scatter_test";
-                                 std::cout << "running scatter_test" << std::endl;
-                                 int result = system(command.c_str());
-                       } else if (pattern.asString() == "allgather") {
-                                 std::string command = "mpirun -n 2 unit_tests/" + library + "_allgather_test";
-                                 std::cout << "running allgather_test" << std::endl;
-                                 int result = system(command.c_str());
-                       } else if (pattern.asString() == "alltoall") {
-                                 std::cout << "running alltoall_test" << std::endl;
-                                 std::string command = "mpirun -n 2 unit_tests/" + library + "_alltoall_test";
-                                 int result = system(command.c_str());
-                       } else if (pattern.asString() == "bcast") {
-                                 std::cout << "running bcast_test" << std::endl;
-                                 std::string command = "mpirun -n 2 unit_tests/" + library + "_bcast_test";
-                                 int result = system(command.c_str());
-                       } else if (pattern.asString() == "xccl" || pattern.asString() == "ipc_put" || pattern.asString() == "ipc_get") {
-                                 std::cout << "running default test" << std::endl;
-				 std::string command = "mpirun -n 2 unit_tests/" + library + "_test";
-                                 int result = system(command.c_str());
-                       }
-                   }
+	    	std::string library = stepNumber["library"].asString();
+		if (library == "mpi") {
+		   platform = library::MPI;
+		} else if (library == "ipc_put") {
+			  platform = library::IPC;
+		} else if (library == "ipc_get") {
+			  platform = library::IPC_get;
+		} else if (library == "xccl") {
+			  platform = library::NCCL;
+		} else {
+		  std::cerr << "Unknown library" << std::endl;
 		}
+		Json::Value patternsArray = stepNumber["patterns"];
+                for (const auto& testType : patternsArray) {
+		    Comm<int> test(platform);
+                    if (testType.asString() == "gather") {
+                       std::cout << "adding gather_test" << std::endl;
+		       patterns.push_back(pattern::gather);
+                    } else if (testType.asString() == "p2p") {
+			      std::cout << "adding p2p_test" << std::endl;
+			      patterns.push_back(pattern::p2p);
+                    } else if (testType.asString() == "scatter") {
+			      std::cout << "adding scatter_test" << std::endl;
+                              patterns.push_back(pattern::scatter);
+                    } else if (testType.asString() == "allgather") {
+			      std::cout << "adding allgather test" << std::endl;
+			      patterns.push_back(pattern::allgather);
+                    } else if (testType.asString() == "alltoall") {
+                              std::cout << "adding alltoall_test" << std::endl;
+		              patterns.push_back(pattern::alltoall);
+                    } else if (testType.asString() == "bcast") {
+			      std::cout << "adding bcast_test" << std::endl;
+			      patterns.push_back(pattern::broadcast);
+                    }
+                }
             }
     }
+    return 0;
 }
